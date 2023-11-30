1d7a51248b53b14f03721f351b410209201b906b
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-11-30 09:16:46 -0800

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
+    n = len(sys.argv)
+    if n == 1:
+	print("0 arguments.")
+    elif n > 1:
+        print(f"{n} arguments:")
+	for i in range(0, n):
+            if i != 0:
+                print({i}: {sys.argv[i]})
