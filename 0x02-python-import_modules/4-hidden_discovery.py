c18e053b916415706d19fe14fe40b08525bf48cb
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-11-30 15:55:59 -0800

    p

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    
-    for i in dir(hidden_4):
+    content = dir(hidden_4)
+    for i in content:
         if i[:2] != "__":
-            print("{:i}".format(i))
+            print(i)
