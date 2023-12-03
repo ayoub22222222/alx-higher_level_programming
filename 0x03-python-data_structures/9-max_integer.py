cce9a9b261bfa4face9f37105e3936a68770b4cd
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-12-03 09:18:17 -0800

    9-max_integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    i = 0
+    j = 1
+    max_value = 0
+    header = my_list[i]
+    current = my_list[j]
+    if len(my_list) == 0:
+	return None
+    while i < len(my_list) and j < len(my_list):
+        if (my_list[i] > my_list[j]) and (my_list[i] > max_value):
+            max_value = my_list[i]
+	elif (my_list[j] > my_list[i]) and (my_list[j] > max_value):
+            max_value = my_list[j]
+	i += 1 
+	j += 1
+    return max_value
