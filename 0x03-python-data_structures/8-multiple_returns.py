64cfdfab76d0d82015a84ca4ff1814c46c9bd6f6
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-12-02 19:09:31 -0800

    p

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,6 @@ def multiple_returns(sentence):
     var = ()
     if len(sentence) == 0:
 	var = 0, "None"
-    counter = 0
     else:
-        for _ in sentence:
-            counter += 1
-        return counter, sentence[0]
+        var =  len(sentence), sentence[0]
+    return var
