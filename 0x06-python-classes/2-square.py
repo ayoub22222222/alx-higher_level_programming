28f5159615aa3774233a668dd4dd2b713f343421
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-12-19 14:58:44 -0800

    p

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,10 @@
 class Square:
     """check if the size match with the requerment"""
     def __init__(self, size=0):
-        if type(self.size) != int:
+        self.__size = size
+        if type(self.__size) != int:
             raise TypeError("size must be an integer")
         elif self.__size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
