a4316dc186a14c6b2a7495bc8a91a7a3381cb2bf
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-12-19 16:30:13 -0800

    p

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,13 +5,7 @@
 class Square:
     """check if the size match with the requerment"""
     def __init__(self, size=0):
-        self.__size = size
-        if type(self.__size) != int:
-            raise TypeError("size must be an integer")
-        elif self.__size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
 
     def area(self):
         """return the size of square"""
