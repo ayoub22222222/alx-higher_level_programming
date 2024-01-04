a55e809eab614fd25808be98d5e9f6b9f6e402fd
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-01-04 15:03:49 -0800

    p

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,6 +17,7 @@ class Rectangle:
         """return the value of width"""
         return self.__width
 
+
     @width.setter
     def width(self, value):
         """set a new value to the width"""
@@ -26,10 +27,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+
     @property
     def height(self):
         """return the value of the height"""
         return self.__height
+
     
     @height.setter
     def height(self, value):
