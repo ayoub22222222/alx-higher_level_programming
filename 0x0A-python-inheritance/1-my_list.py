48962dbef1b1e3a05197c4d36be59dba80865252
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-01-08 15:43:37 -0800

    1-my_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this module is contain two class one
+iherente behvior from the other
+"""
+
+
+class Mylist(list):
+    """this class iherente methode
+    attribute from class list
+    """
+    def print_sorted(self):
+        """print sorted list"""
+        print(sorted(self))
