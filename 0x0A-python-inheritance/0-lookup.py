f440fe8b06a26780faf2065122462ef48ec64209
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-01-08 15:30:33 -0800

    p

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,13 +3,8 @@
 this function is return the all the atributes inside an object
 """
 
-def lookup(obj):
-    """this funtion take one parameter as 
-        an argument
-        args:
-            parameter1: object
-        return: 
-            all the attribute and methode on that object
-    """
-    return dir(obj)
 
+def lookup(obj):
+    """this funtion take one parameter as
+    an argument"""
+    return dir(obj)
