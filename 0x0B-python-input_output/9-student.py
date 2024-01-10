c9b7ba8df59d141c958a95218c7a77813c4204e8
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-01-10 01:13:16 +0000

    q

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+
+this module contain a class that return a dictionary
+representation of an object
+"""
+
+
+class Student():
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with full_name las_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns dictionary
+        """
+        return self.__dict__
