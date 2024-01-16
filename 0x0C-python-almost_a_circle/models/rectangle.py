a036f935174ccf89ec9eca12ced96a98fbb7afa8
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-01-15 16:56:20 -0800

    p

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,18 +10,13 @@ class Rectangle(Base):
     """Represent a rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
-        Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
+        """this class take four parameter as an argument
+        args:
+            parameter1: integer value
+            parameter2: int value
+            parameter3: int value
+            parameter4: int value
+            parameter5: None value
         """
         self.width = width
         self.height = height
@@ -80,3 +75,6 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+    def area(self):
+        """claculate the area of the rectangle"""
+        return self.width * self.height
