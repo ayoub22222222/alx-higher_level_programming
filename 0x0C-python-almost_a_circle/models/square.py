821f22631631e8e81d477e74d3aa4d0a024b404f
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-01-15 17:52:39 -0800

    lalaland

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module contain a class
+square
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    defines class Square that inherent from
+    a class Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """Prints [Square] (<id>) <x>/<y> - <size>"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.size)
