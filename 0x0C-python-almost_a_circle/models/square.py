ac246de85fea995e657670e558f10f74ca41b0e8
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-01-15 17:57:04 -0800

    lalaland2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,3 +23,48 @@ class Square(Rectangle):
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
             self.__class__.__name__, self.id, self.x, self.y,
             self.size)
+
+    @property
+    def size(self):
+        """return the width"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set a new value"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        update the Rectangle value
+        """
+        if args:
+            for k, v in enumerate(args):
+                if k == 0:
+                    self.id = v
+                elif k == 1:
+                    self.size = v
+                elif k == 2:
+                    self.x = v
+                else:
+                    self.y = v
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """return a dict representation of
+        the rectangle information"""
+        d = {}
+        d["id"] = self.id
+        d["size"] = self.size
+        d["x"] = self.x
+        d["y"] = self.y
+        return d
