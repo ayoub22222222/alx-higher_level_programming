8557f999fabeec7a510463e52ce828a798dbc796
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-01-19 12:17:48 -0800

    oo

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,16 +13,13 @@ class TestBaseClass(unittest.TestCase):
     use the TestCase methode to test different type of
     scenario including success and flr ones
     """
-    def test_random_input(self):
-        pass
-
     def test_empty_base(self):
         empty1 = Base()
         empty2 = Base()
         empty3 = Base()
-        self.assertEqual(print(empty1.id), 1)
-        self.assertEqual(print(empty2.id), 2)
-        self.assertEqual(print(empty3.id), 3)
+        self.assertEqual(empty1.id, 1)
+        self.assertEqual(empty2.id, 2)
+        self.assertEqual(empty3.id, 3)
 
     def test_basic_int_input(self):
         """this methode test tree
@@ -30,51 +27,43 @@ class TestBaseClass(unittest.TestCase):
         basic1 = Base(14)
         basic2 = Base(15)
         basic3 = Base(111)
-        self.assertEqual(print(basic1.id), 14)
-        self.assertEqual(print(basic2.id), 15)
-        self.assertEqual(print(basic3.id), 111)
-
-    def test_basic_str_input(self):
-        """test string input"""
-        pass
+        self.assertEqual(basic1.id, 14)
+        self.assertEqual(basic2.id, 15)
+        self.assertEqual(basic3.id, 111)
 
     def test_large_number(self):
         """test large number"""
         large = Base(100000000000000)
-        self.assertEqual(print(large.id), 100000000000000),
-    
+        self.assertEqual(large.id, 100000000000000),
+
     def test_bolean_value(self):
         """test bolean value"""
         bol1 = Base(True)
         bol2 = Base(False)
-        self.assertFalse(print(bol2.id), False)
-        self.assertTrue(print(bol1.id), True)
+        self.assertFalse(bol2.id, False)
+        self.assertTrue(bol1.id, True)
 
     def test_None_value(self):
         """test None input"""
         n = Base(None)
-        self.asserIsNone(print(n.id), "expected to be None")
-        
+        self.asserIsNone(n.id, "expected to be None")
+
     def test_list_input(self):
         """test list input"""
         lst = Base([1, 2, 3])
-        self.assertEqual(print(lst.id), [1, 2, 3])
-    
+        self.assertEqual(lst.id, [1, 2, 3])
+
     def test_dict_value(self):
         """test dictionnairies input"""
         dct = Base({"value_one": 1, "value_two": 2})
-        self.assertEqual(print(dct.id), {"value_one": 1, "value_two": 2})
+        self.assertEqual(dct.id, {"value_one": 1, "value_two": 2})
 
     def test_tuple_input(self):
         """test tuple input"""
         self.assertEqual(Base((1, 2, 3), (1, 2, 3))
-    
 
     def test_syntax_error(self):
         """test syntax error"""
         syn = Base($)
         with self.assertRaises(SyntaxError):
             print(Base($).id)
-
-if __name__ == "__main__":
-    unittest.main()
