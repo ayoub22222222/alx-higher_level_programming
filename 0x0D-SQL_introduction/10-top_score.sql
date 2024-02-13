be4062e560ee35edb2571de774050adee2124077
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-13 11:41:08 -0800

    no cheating

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,11 @@
 -- script that lists all records of the table second_table 
 -- of the database hbtn_0c_0 in your MySQL server.
-SELECT id, score FROM second_table ORDER BY score DESC;
+SELECT 
+	id, score 
+FROM 
+	second_table 
+ORDER BY 
+	score 
+DESC;
 
 
