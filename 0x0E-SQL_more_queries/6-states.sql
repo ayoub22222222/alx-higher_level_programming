49d1ccd158b47dfd31dba822f6458e853e10b166
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-14 17:20:07 -0800

    not all at once

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- later on
+USE hbtn_0d_usa;
+CREATE table IF NOT EXISTS states(
+       id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+       name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,6 @@
+# SQL more queries
+
+# Description
+
+# Installation
+
