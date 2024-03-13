d04e2f321feb3e2b08a1a3bcb5ed53358ba6e5e9
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-03-12 19:30:39 -0700

    task 8

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let nOccurrences = 0;
-  let xvar = 0;
-  while (xvar <= list.length) {
-	  if (list[xvar] === searchElement) {
-		  nOccurrences++;
-	  }
-	  xvar++;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
+    }
   }
   return nOccurrences;
 };
