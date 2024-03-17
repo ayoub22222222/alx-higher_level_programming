04ea3be86a9f9580ff16f4b9fb935a7258b710d2
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-03-17 16:43:41 -0700

    task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,14 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+
+    db = MySQLdb.connect(host= "localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+
+    curr = db.cursor()
+
+    curr.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for i in curr.fetchall():
+        print(i)
+    curr.close()
     db.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+decription of the task
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    curr = db.cursor()
+    curr.execute("SELECT * FROM states WHERE name LIKE N% ORDER BY states.id ASC")
+    elements = curr.fetchall()
+    for element in curr.elements:
+        print(element)
+    cur.close()
+    db.close()
