9d634326843754169b4a4fa19336eaf5d3972628
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-03-21 10:02:19 -0700

    task 100_2

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     cn_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
     engine = create_engine(cn_db, pool_pre_ping=True)
 
-    Base.metadata.cerate_all(engine)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     n_state = State(name='California')
-    n_city = Cityname='San Francisco')
+    n_city = City(name='San Francisco')
     n_state.cities.append(n_city)
 
     session.add(n_state)
