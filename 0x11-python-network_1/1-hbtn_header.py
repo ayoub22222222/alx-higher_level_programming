ed6475dbd5792de90f37cbafc8ba902396d0120c
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-03-28 10:30:25 -0700

    task fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,8 @@
 
 import urllib.request
 from sys import argv
+if __name__ == "__main__":
 
-with urllib.request.urlopen(argv[1]) as response:
-    rd = response.info()
-    print(rd['X-Request-Id'])
+    with urllib.request.urlopen(argv[1]) as response:
+        rd = response.info()
+        print(rd['X-Request-Id'])
