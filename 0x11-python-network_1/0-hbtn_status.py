3ed31664825dee5f90040e01033d38ebb7af8bee
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-03-28 10:03:35 -0700

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""description"""
+
+import urllib.request
+
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    rd = response.read()
+    print("Body response:")
+    print("\t - type:{}".format(rd))
+    print("\t - content: {}".format(rd))
+    print("\t - utf8 content: {}".format(rd.decode('utf8')))
+
