eb80ce0ef79729bf122f8075e7071c9c7f53ac1e
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-03-27 18:42:08 -0700

    task 4/5

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# file description
+curl -sH "X-School-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# description file
+curl -sL "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
