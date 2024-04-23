8b273a68ead12b94ad7621afaf7addf80d6d3cb1
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-04-23 16:29:37 -0700

    fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-fs.writeFile(`${arg1}`, arg2 + '\n', 'utf8', function (err) {
+fs.writeFile(`${arg1}`, arg2, 'utf8', function (err) {
   if (err) {
     console.error(err);
   }
