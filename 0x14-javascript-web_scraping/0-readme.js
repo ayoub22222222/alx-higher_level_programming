1224e3ba8bd7d823d4797da6cdd15fe2063c9d37
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-04-23 11:23:27 -0700

    task fix 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,10 @@
 const fs = require('fs');
 const arg1 = process.argv[2];
 
-fs.readFile(`${arg1}`, 'utf8', function(err, data) {
-	console.log(data);
-	if (err) {
-		console.error(err);
-	}
+fs.readFile(`${arg1}`, 'utf8', function (err, data) {
+  console.log(data);
+
+  if (err) {
+    console.error(err);
+  }
 });
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -1,1 +1,3 @@
 this file is for learning purpose
+[![js-semistandard-style](https://raw.githubusercontent.com/standard/semistandard/master/badge.svg)](https://github.com/standard/semistandard)
+
